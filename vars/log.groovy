c4366467668915c35fdc3d8e820bf07cc115e03c
singuvenkatesh <singuvenkatesh@gmail.com>
2019-01-31 16:27:41 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,37 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
-def warning(message) {
-    echo "WARNING: ${message}"
+    node {
+	    // Clean workspace before doing anything
+	    deleteDir()
+
+	    try {
+	        stage ('Clone') {
+	        	checkout scm
+	        }
+	        stage ('Build') {
+	        	sh "echo 'building ${config.projectName} ...'"
+	        }
+	        stage ('Tests') {
+		        parallel 'static': {
+		            sh "echo 'shell scripts to run static tests...'"
+		        },
+		        'unit': {
+		            sh "echo 'shell scripts to run unit tests...'"
+		        },
+		        'integration': {
+		            sh "echo 'shell scripts to run integration tests...'"
+		        }
+	        }
+	      	stage ('Deploy') {
+	            sh "echo 'deploying to server ${config.serverDomain}...'"
+	      	}
+	    } catch (err) {
+	        currentBuild.result = 'FAILED'
+	        throw err
+	    }
+    }
 }
